7327ee4a26be3e6438e0287651efcf086b6622a8
Simos <simos@cdmania.eu>
2026-05-26 13:05:34 +0300

    Dto's  for Products and install automapper for controller

---
+++ Application/DTOs/ProductResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Eshop.Application.DTOs
+{
+    public class ProductResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
